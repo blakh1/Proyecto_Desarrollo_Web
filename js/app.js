bf79dc0e3845acb4c7c1baeb0408217bac3fece6
blakh1 <s.deleon.a@hotmail.com>
2023-11-01 19:55:38 -0300

    repositorie actualiz

---
+++ js/app.js
@@ -0,0 +1,92 @@
+let carsContainer = document.querySelector("#carsContainer");
+
+fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (cars) {
+        for (const car of cars) {
+            carsContainer.insertAdjacentHTML(
+                "beforeend",
+                `<div class="row border-bottom mb-4 pb-4">
+                            <div class="col-12 col-lg-4">
+                                <div class="position-relative">
+                                    <img
+                                        src="${car.image}"
+                                        class="img-fluid border p-2 mb-3 mb-lg-0"
+                                        alt=".."
+                                    />
+                                    <div
+                                        class="new position-absolute px-2 rounded spanNew d-none"
+                                    >
+                                        <strong class="text-white"
+                                            >Nuevo</strong
+                                        >
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-8 d-flex flex-column">
+                                <div class="card-body">
+                                    <div
+                                        class="d-flex justify-content-between align-items-center"
+                                    >
+                                        <h3 class="card-title carName">
+                                        ${car.brand + " " + car.model} 
+                                        </h3>
+                                        <div class="datosDelAuto d-flex">
+                                            <p class="year me-1 my-0">${
+                                                car.year
+                                            }</p>
+                                            |
+                                            <p class="price mx-1 my-0">
+                                            ${"USD " + car.price_usd}
+                                            </p>
+                                            |
+                                            <p class="stars ms-1 my-0">
+                                                <i class="bi bi-star-fill"></i
+                                                ><i class="bi bi-star-fill"></i
+                                                ><i class="bi bi-star-fill"></i
+                                                ><i class="bi bi-star-half"></i
+                                                ><i class="bi bi-star"></i>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <p class="card-text my-3">
+                                    ${car.description}
+                                    </p>
+                                </div>
+                                <div class="buttons">
+                                    <button
+                                        class="btn saleButton"
+                                        type="submit"
+                                    >
+                                        <i class="bi bi-cart3"></i> Comprar
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="btn btn-outline-dark"
+                                    >
+                                        <i class="bi bi-plus-square"></i> Mas
+                                        información
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="btn btn-outline-dark"
+                                    >
+                                        <i class="bi bi-share"></i> Compartir
+                                    </button>
+                                </div>
+                            </div>
+                        </div>`
+            );
+            let nuevo = document.querySelectorAll(".spanNew");
+            if (car.status === 1) {
+                nuevo[nuevo.length - 1].classList.remove("d-none");
+            }
+
+            let stars = document.querySelectorAll(".stars");
+            for (let i = 0; i < car.rating; i++) {
+                const element = array[i];
+            }
+        }
+    });
